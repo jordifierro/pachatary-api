f8e49299c46e37c2f6e5af7e7a6c52b3f63f9f5e
jordifierro <jordifierromulero@gmail.com>
2018-11-15 17:32:37 +0100

    Add robots.txt to avoid requests from robots

--- pachatary/urls.py
+++ pachatary/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.http import HttpResponse
 from django.urls import include, path
 from django.contrib import admin
 from django.conf import settings
@@ -24,6 +25,7 @@ urlpatterns = [
     url(r'^apple-app-site-association$', aasa_redirect, name='aasa'),
     url(r'^privacy-policy$', privacy_policy, name='privacy-policy'),
     url(r'^terms-and-conditions$', terms_and_conditions, name='terms-and-conditions'),
+    url(r'^robots.txt$', lambda r: HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain'))
 ]
 
 if settings.LOCAL_DEPLOY:
